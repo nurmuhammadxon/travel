2138f5e53655599a0b350cc74e01d669f655fa49
nurmuhammadxon <muhiddinovnurmuhammadxon1@gmail.com>
2026-08-20 10:00:36 +0500

    fix locale files tracing for vercel

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
     devIndicators: false,
     outputFileTracingIncludes: {
-        "/[lng]/**": ["./public/locales/**"],
+        "/**": ["./public/locales/**"],
     },
 };
 
